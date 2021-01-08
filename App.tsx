0d060ea4b802b8a8e5fa6ce1281ef4f1baba5b04
Karthik Balasubramanian <kabalas@microsoft.com>
2021-01-07 23:21:54 -0800

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { StyleSheet, View } from "react-native";
+import Home from "./components/Home";
+import { registerForPushNotifications } from "./service/Notifications";
 
-export default function App() {
+const App = () => {
+  useEffect(() => {
+    registerForPushNotifications();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.scrollView}>
+        <Home />
+        <StatusBar style="auto" />
+      </View>
+    </>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  scrollView: {
+    display: "flex",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    backgroundColor: "#fff",
   },
 });
+
+export default App;
